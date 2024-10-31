986654fa162d90afa1d068ee704854286ffde14b
shahinsk17 <shahinshaikh6828@gmil.com>
2024-10-31 18:59:00 +0530

    changes

--- Donation_Project/DonationProject_Backend/index.js
+++ Donation_Project/DonationProject_Backend/index.js
@@ -8,17 +8,22 @@ import messageRouter from "./router/messageRouter.js"
 
 const app=express();
 dotenv.config({path: "./config.env"});
+ 
+
+
 app.use(cors({
-    origin:[process.env.FRONTEND_URL],methods:["POST"],
-    credentials:true,
-}))
+    origin: ['http://localhost:3000', 'https://unite-for-change-mern-stack-project.vercel.app'],
+    methods: ["POST"],
+    credentials: true,
+}));
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use("/api/v1/message",messageRouter);
 
-app.use(cors({ origin: 'https://unite-for-change-mern-stack-project.vercel.app' }));
+ 
 
 
 dbConnection();
